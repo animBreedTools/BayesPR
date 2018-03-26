80519ea630f568338f63ef91ece8a5548a38b273
Emre Karaman <emrkaraman@gmail.com>
2018-03-27 00:02:37 +0200

    Update dataPrepare.jl

--- dataPrepare.jl
+++ dataPrepare.jl
@@ -63,7 +63,7 @@ function simPheno(popGeno,h2_1,h2_2,meanMaf,q1QTLs,q2QTLs,q12QTLs)
     
     infoSimQTL = DataFrame(zeros((size(popGeno,2)-1),5))
     println(names(popGeno)[selectedLoci])
-    infoSimQTL[:,1] .= names(popGeno)[selectedLoci]
+    infoSimQTL[:,1] .= QTLs-1
     infoSimQTL[:,2] .= QTLs-1
     infoSimQTL[(vcat(q1QTLs,q12QTLs)-1),3] .= 1
     infoSimQTL[(vcat(q12QTLs,q2QTLs)-1),4] .= 1

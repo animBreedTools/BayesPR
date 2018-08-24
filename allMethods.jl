2326148b131492282ed6da52c41afe9c59605302
Emre Karaman <emrkaraman@gmail.com>
2018-08-24 11:12:10 +0200

    Update allMethods.jl

--- allMethods.jl
+++ allMethods.jl
@@ -346,7 +346,7 @@ function mtJWAS(phenoDataInRef::DataFrame,phenoDataInVal::DataFrame,genoData_All
         println("size of var1 $(size(var1))")
         var1    = reshape(var1,size(genoTest,2),Int(nChain/nThin))[nBurnin+1:end,:] #
         var2    = reshape(var2,size(genoTest,2),Int(nChain/nThin))[nBurnin+1:end,:]# Hao's JWAS prints out everything.
-        coVar12 = reshape(covar12,size(genoTest,2),Int(nChain/nThin))[nBurnin+1:end,:]
+        coVar12 = reshape(coVar12,size(genoTest,2),Int(nChain/nThin))[nBurnin+1:end,:]
         println("size of var1 $(size(var1))")
         meanVar1    = mean(var1,dims=2)
         meanVar2    = mean(var2,dims=2)
